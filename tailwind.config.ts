92f7bce07d5d4acc0a9512d3e75134097b75f5e7
heynar-max <heynar76@hotmail.com>
2024-11-26 22:22:34 -0500

    Top Menu

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      screens: {
+        'custom-800': '800px',  // Aquí agregas el nuevo breakpoint personalizado
+      },
     },
   },
   plugins: [],

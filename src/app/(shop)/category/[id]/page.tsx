a3a813d22db26f5cbb1500619ad676a2a0193295
heynar-max <heynar76@hotmail.com>
2024-11-26 19:45:33 -0500

    Creando rutas de la aplicación

---
+++ src/app/(shop)/category/[id]/page.tsx
@@ -0,0 +1,8 @@
+
+export default function categoryIdShopPage() {
+    return (
+        <div>
+        <h1>category Page</h1>
+        </div>
+    );
+}
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -2,9 +2,9 @@
 
 export default function Home() {
   return (
-      <main>
+      <div>
         <h1>hola mundo</h1>
-      </main>
+      </div>
     
   );
 }

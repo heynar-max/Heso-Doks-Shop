e3db60e36eb8aa6a6499b0d4d16ba8f9dee7e7b4
heynar-max <heynar76@hotmail.com>
2024-12-05 22:00:46 -0500

    Grid de Productos

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,5 +1,7 @@
-import { Title } from "@/components";
+import { ProductGrid, Title } from "@/components";
+import { initialData } from '../../seed/seed';
 
+const products = initialData.products;
 
 export default function Home() {
   return (
@@ -8,6 +10,10 @@ export default function Home() {
           title="Tienda"
           subtitle="Todos los productos"
           className="mb-2"
+        />
+
+        <ProductGrid
+          products={ products }
         />
       </>
     
